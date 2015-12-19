a1bbf35ae8f4166f13e87331cf3d54ee4d90619c
Thomas Hope <hope.thomasj@gmail.com>
2015-12-19 11:53:35 +0000

    connected the hand controlls

--- Tabbed_picker.py
+++ Tabbed_picker.py
@@ -15,7 +15,6 @@ cmds.formLayout(form_layout, edit=True, attachForm=((tab_layout, 'top', 0),
                                                     (tab_layout, 'left', 0),
                                                     (tab_layout, 'bottom', 0),
                                                     (tab_layout, 'right', 0)) )
-
 ## Setup the model picker tab
 model_tab = cmds.formLayout(width=200, height=200)
 
@@ -33,35 +32,55 @@ cmds.formLayout(model_tab, e=True, af=[[resetall_button,"left",WIDTH()/2+10],[re
 
 ## Setup the foot tab
 cmds.setParent( tab_layout )
-feet_tab = cmds.rowColumnLayout(numberOfColumns=3,
-                                columnWidth=[(1, WIDTH()/5), (2, WIDTH()/5*3), (3, WIDTH()/5)],
-                                columnAttach=[(1, 'both', 5), (2, 'both', 5), (3, 'both', 10)])
+feet_tab = cmds.columnLayout( width=WIDTH() )
 
-cmds.text(label="Heel Lift", align="right")
-hl_slider = cmds.floatSlider(min=-20, max=20, value=0, step=0.1)
-hl_field = cmds.floatField()
-
-cmds.connectControl( hl_slider, '%s.Custom' % 'pSphere1' )
-
-cmds.text(label="Toe Lift", align="right")
-tl_slider = cmds.floatSlider(min=-20, max=20, value=0, step=0.1)
-cmds.button(label="Reset", command='cmds.floatSlider(tl_slider,e=True,v=0)')
-
-cmds.setParent( tab_layout )
-cmds.columnLayout()
-
-fl_slider = cmds.floatSliderGrp( label='Group 1', field=True )
-cmds.connectControl( fl_slider, '%s.tx' % 'pSphere1' )
+hl_grp = cmds.floatSliderGrp( l='Heel Lift', field=True, min=-20, max=20, value=0, cw=[(1,80),(2,60),(3,WIDTH()-160)] )
+#cmds.connectControl( hl_grp, '%s.Custom' % 'pSphere1' )
 
 
 ## Setup the hands tab
 cmds.setParent( tab_layout )
-hands_tab = cmds.rowColumnLayout(numberOfColumns=2)
+hands_tab = cmds.columnLayout( width=WIDTH(), adjustableColumn=True )
 
-cmds.button()
-cmds.button()
-cmds.button()
-cmds.intSlider()
+cmds.separator(style="in", height=10)
+cmds.text(label="Left Hand", align="center", height=20)
+cmds.separator(style="out", height=10)
+
+# Setup sliders for left hand attributes
+acl_grp = cmds.floatSliderGrp( l='All Curl', field=True, min=-20, max=20, value=0, cw=[(1,80),(2,60),(3,WIDTH()-160)] )
+cmds.connectControl( acl_grp, '%s.All_Curl' % 'ctrl_L_arm_wrist' )
+pcl_grp = cmds.floatSliderGrp( l='Pinky Curl', field=True, min=-20, max=20, value=0, cw=[(1,80),(2,60),(3,WIDTH()-160)] )
+cmds.connectControl( pcl_grp, '%s.Pinky_Curl' % 'ctrl_L_arm_wrist' )
+mcl_grp = cmds.floatSliderGrp( l='Middle Curl', field=True, min=-20, max=20, value=0, cw=[(1,80),(2,60),(3,WIDTH()-160)] )
+cmds.connectControl( mcl_grp, '%s.Mid_Curl' % 'ctrl_L_arm_wrist' )
+icl_grp = cmds.floatSliderGrp( l='Index Curl', field=True, min=-20, max=20, value=0, cw=[(1,80),(2,60),(3,WIDTH()-160)] )
+cmds.connectControl( icl_grp, '%s.Index_Curl' % 'ctrl_L_arm_wrist' )
+tcl_grp = cmds.floatSliderGrp( l='Thumb Curl', field=True, min=-20, max=20, value=0, cw=[(1,80),(2,60),(3,WIDTH()-160)] )
+cmds.connectControl( tcl_grp, '%s.Thumb_Curl' % 'ctrl_L_arm_wrist' )
+tsi_grp = cmds.floatSliderGrp( l='Thumb Side', field=True, min=-20, max=20, value=0, cw=[(1,80),(2,60),(3,WIDTH()-160)] )
+cmds.connectControl( tsi_grp, '%s.Thumb_Side' % 'ctrl_L_arm_wrist' )
+spr_grp = cmds.floatSliderGrp( l='Spread', field=True, min=-20, max=20, value=0, cw=[(1,80),(2,60),(3,WIDTH()-160)] )
+cmds.connectControl( spr_grp, '%s.Spread' % 'ctrl_L_arm_wrist' )
+
+cmds.separator(style="in", height=10)
+cmds.text(label="Right Hand", align="center", height=20)
+cmds.separator(style="out", height=10)
+
+# Setup sliders for right hand attributes
+acl_grp = cmds.floatSliderGrp( l='All Curl', field=True, min=-20, max=20, value=0, cw=[(1,80),(2,60),(3,WIDTH()-160)] )
+cmds.connectControl( acl_grp, '%s.All_Curl' % 'ctrl_R_arm_wrist' )
+pcl_grp = cmds.floatSliderGrp( l='Pinky Curl', field=True, min=-20, max=20, value=0, cw=[(1,80),(2,60),(3,WIDTH()-160)] )
+cmds.connectControl( pcl_grp, '%s.Pinky_Curl' % 'ctrl_R_arm_wrist' )
+mcl_grp = cmds.floatSliderGrp( l='Middle Curl', field=True, min=-20, max=20, value=0, cw=[(1,80),(2,60),(3,WIDTH()-160)] )
+cmds.connectControl( mcl_grp, '%s.Mid_Curl' % 'ctrl_R_arm_wrist' )
+icl_grp = cmds.floatSliderGrp( l='Index Curl', field=True, min=-20, max=20, value=0, cw=[(1,80),(2,60),(3,WIDTH()-160)] )
+cmds.connectControl( icl_grp, '%s.Index_Curl' % 'ctrl_R_arm_wrist' )
+tcl_grp = cmds.floatSliderGrp( l='Thumb Curl', field=True, min=-20, max=20, value=0, cw=[(1,80),(2,60),(3,WIDTH()-160)] )
+cmds.connectControl( tcl_grp, '%s.Thumb_Curl' % 'ctrl_R_arm_wrist' )
+tsi_grp = cmds.floatSliderGrp( l='Thumb Side', field=True, min=-20, max=20, value=0, cw=[(1,80),(2,60),(3,WIDTH()-160)] )
+cmds.connectControl( tsi_grp, '%s.Thumb_Side' % 'ctrl_R_arm_wrist' )
+spr_grp = cmds.floatSliderGrp( l='Spread', field=True, min=-20, max=20, value=0, cw=[(1,80),(2,60),(3,WIDTH()-160)] )
+cmds.connectControl( spr_grp, '%s.Spread' % 'ctrl_R_arm_wrist' )
 
 # Name the tabs
 cmds.setParent( tab_layout )
