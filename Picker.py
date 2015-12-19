b4614ded2496adf9aaa0dee74dd3003a4fb35fde
Thomas Hope <hope.thomasj@gmail.com>
2015-12-19 17:59:00 +0000

    moving on with picker without bg image

--- Picker.py
+++ Picker.py
@@ -12,38 +12,30 @@ cmds.window(widthHeight=(WIDTH(), HEIGHT()), title="Model Picker", sizeable=Fals
 # Set the layout then set the parent back to the window
 form_layout = maya.cmds.formLayout()
 
-
 # Load the image from Documents/maya/2015-x64/prefs/icons
-background_image = cmds.image(image="hellknight.png")
+background_image = cmds.image(image="HellknightBG.png", width=WIDTH(), height=HEIGHT())
     
 # Create the controls,
-r_wrist_button = cmds.button(label="", w=20,  h=20, bgc=(0.2, 0.2, 1.0), command="pass")
-l_wrist_button = cmds.button(label="", w=20,  h=20, bgc=(1.0, 0.2, 0.2), command="pass")
-r_leg_button   = cmds.button(label="", w=20,  h=20, bgc=(0.2, 0.2, 1.0), command="pass")
-l_leg_button   = cmds.button(label="", w=20,  h=20, bgc=(1.0, 0.2, 0.2), command="pass")
-pelvis_button  = cmds.button(label="", w=70, h=10, bgc=(1.0, 1.0, 1.0), command="pass")
+r_wrist_button = cmds.button(label="", w= 20, h=20, bgc=(0.2, 0.2, 1.0), command="pass")
+l_wrist_button = cmds.button(label="", w= 20, h=20, bgc=(1.0, 0.2, 0.2), command="pass")
+r_leg_button   = cmds.button(label="", w= 20, h=20, bgc=(0.2, 0.2, 1.0), command="pass")
+l_leg_button   = cmds.button(label="", w= 20, h=20, bgc=(1.0, 0.2, 0.2), command="pass")
+pelvis_button  = cmds.button(label="", w= 70, h=10, bgc=(1.0, 1.0, 1.0), command="pass")
 master_button  = cmds.button(label="", w=200, h=10, bgc=(1.0, 1.0, 1.0), command="pass")
 
 # set the parent back to the window so we can possiton our objects on the form
 #cmds.setParent("..")
 
 # Position the image in the layout
-cmds.formLayout(form_layout, edit=True,
-    attachForm=[[background_image, "left", 78],[background_image, "top", 110]])
+cmds.formLayout(form_layout, edit=True, attachForm=[[background_image, "left", 0],[background_image, "top", 0]])
     
 # Position the controls
-cmds.formLayout(form_layout, edit=True,
-    attachForm=[[r_wrist_button,"left",100],[r_wrist_button,"top",220]])
-cmds.formLayout(form_layout, edit=True,
-    attachForm=[[l_wrist_button,"left",380],[l_wrist_button,"top",220]])
-cmds.formLayout(form_layout, edit=True,
-    attachForm=[[r_leg_button,"left",200],[r_leg_button,"top",400]])
-cmds.formLayout(form_layout, edit=True,
-    attachForm=[[l_leg_button,"left",280],[l_leg_button,"top",400]])
-cmds.formLayout(form_layout, edit=True,
-    attachForm=[[pelvis_button,"left",WIDTH()/2-35],[pelvis_button,"top",250]])
-cmds.formLayout(form_layout, edit=True,
-    attachForm=[[master_button,"left",WIDTH()/2-100],[master_button,"top",480]])
+cmds.formLayout(form_layout, e=True, af=[[r_wrist_button,"left",100          ],[r_wrist_button,"top",220]])
+cmds.formLayout(form_layout, e=True, af=[[l_wrist_button,"left",380          ],[l_wrist_button,"top",220]])
+cmds.formLayout(form_layout, e=True, af=[[r_leg_button,  "left",200          ],[r_leg_button,  "top",400]])
+cmds.formLayout(form_layout, e=True, af=[[l_leg_button,  "left",280          ],[l_leg_button,  "top",400]])
+cmds.formLayout(form_layout, e=True, af=[[pelvis_button, "left",WIDTH()/2-35 ],[pelvis_button, "top",250]])
+cmds.formLayout(form_layout, e=True, af=[[master_button, "left",WIDTH()/2-100],[master_button, "top",480]])
 
 # display the window
 cmds.showWindow()
--- Tabbed_picker.py
+++ Tabbed_picker.py
@@ -4,48 +4,75 @@ def WIDTH():
     return 400
     
 def HEIGHT():
-    return 600
+    return 500
     
 # Setup the window
 window_name = cmds.window(widthHeight=(WIDTH(), HEIGHT()), title="Hellknight Picker", sizeable=False)
-
-form_layout = cmds.formLayout()
-
 tab_layout = cmds.tabLayout(innerMarginWidth=5, innerMarginHeight=5)
-cmds.formLayout(form_layout, edit=True, attachForm=((tab_layout, 'top', 0),
-                                                    (tab_layout, 'left', 0),
-                                                    (tab_layout, 'bottom', 0),
-                                                    (tab_layout, 'right', 0)) )
 
-## Setup the model picker tab
+## Setup the model picker tab ##
 model_tab = cmds.formLayout(width=WIDTH()-20, height=HEIGHT()-80)
+    
+# Create the right controls
+r_shoulder_btn  = cmds.iconTextButton(w= 20, h=20, bgc=(0.2, 0.2, 1.0), command="pass")
+r_elbow_btn     = cmds.iconTextButton(w= 20, h=20, bgc=(0.2, 0.2, 1.0), command="pass")
+r_wrist_btn     = cmds.iconTextButton(w= 20, h=20, bgc=(0.2, 0.2, 1.0), command="pass")
+r_knee_btn      = cmds.iconTextButton(w= 20, h=20, bgc=(0.2, 0.2, 1.0), command="pass")
+r_leg_btn       = cmds.iconTextButton(w= 20, h=20, bgc=(0.2, 0.2, 1.0), command="pass")
+# Create the left controls
+l_shoulder_btn  = cmds.iconTextButton(w= 20, h=20, bgc=(1.0, 0.2, 0.2), command="left")
+l_elbow_btn     = cmds.iconTextButton(w= 20, h=20, bgc=(1.0, 0.2, 0.2), command="left")
+l_wrist_btn     = cmds.iconTextButton(w= 20, h=20, bgc=(1.0, 0.2, 0.2), command="left")
+l_knee_btn      = cmds.iconTextButton(w= 20, h=20, bgc=(1.0, 0.2, 0.2), command="left")
+l_leg_btn       = cmds.iconTextButton(w= 20, h=20, bgc=(1.0, 0.2, 0.2), command="left")
+# Create the mid controls
+spine_btn       = cmds.iconTextButton(w= 60, h=10, bgc=(1.0, 1.0, 1.0), command="pass")
+waist_btn       = cmds.iconTextButton(w= 70, h=12, bgc=(1.0, 1.0, 1.0), command="pass")
+pelvis_btn      = cmds.iconTextButton(w= 90, h=12, bgc=(1.0, 1.0, 1.0), command="pass")
+master_btn      = cmds.iconTextButton(w=200, h=12, bgc=(1.0, 1.0, 1.0), command="pass")
 
-# Load the image from Documents/maya/2015-x64/prefs/icons on Windows
-# or from ~/Library/Preferences/Autodesk/maya/2015-x64/prefs/icons on OSX
-background_image = cmds.image(image="HellknightBG.png")
+# Position the right controls
+cmds.formLayout(model_tab, e=True, af=[[r_shoulder_btn, "right",WIDTH()/2+20],
+                                       [r_shoulder_btn, "top",      60]])
+cmds.formLayout(model_tab, e=True, af=[[r_elbow_btn,    "right",WIDTH()/2+50],
+                                       [r_elbow_btn,    "top",      80]])
+cmds.formLayout(model_tab, e=True, af=[[r_wrist_btn,    "right",WIDTH()/2+90],
+                                       [r_wrist_btn,    "top",      120]])
+cmds.formLayout(model_tab, e=True, af=[[r_knee_btn,     "right",WIDTH()/2+40],
+                                       [r_knee_btn,     "bottom",   130]])
+cmds.formLayout(model_tab, e=True, af=[[r_leg_btn,      "right",WIDTH()/2+40],
+                                       [r_leg_btn,      "bottom",   80]])
+# Position the left controls
+cmds.formLayout(model_tab, e=True, af=[[l_shoulder_btn, "left", WIDTH()/2+20],
+                                       [l_shoulder_btn, "top",      20]])
+cmds.formLayout(model_tab, e=True, af=[[l_elbow_btn,    "left", WIDTH()/2+50],
+                                       [l_elbow_btn,    "top",      60]])
+cmds.formLayout(model_tab, e=True, af=[[l_wrist_btn,    "left", WIDTH()/2+90],
+                                       [l_wrist_btn,    "top",      100]])
+cmds.formLayout(model_tab, e=True, af=[[l_knee_btn,     "left", WIDTH()/2+40],
+                                       [l_knee_btn,     "bottom",   130]])
+cmds.formLayout(model_tab, e=True, af=[[l_leg_btn,      "left", WIDTH()/2+40],
+                                       [l_leg_btn,      "bottom",   80]])
 
-pelvis_button = cmds.button(label="", w= 70, h=10, bgc=(1.0, 1.0, 1.0), command="pass")
-master_button = cmds.button(label="", w=200, h=10, bgc=(1.0, 1.0, 1.0), command="pass")
-
-cmds.formLayout(model_tab, e=True, af=[[background_image, "left", 0],[background_image, "top", 0]])
-cmds.formLayout(model_tab, e=True, af=[[pelvis_button,"left",WIDTH()/2- 35],[pelvis_button,"top",250]])
-cmds.formLayout(model_tab, e=True, af=[[master_button,"left",WIDTH()/2-100],[master_button,"top",480]])
+cmds.formLayout(model_tab, e=True, af=[[spine_btn,  "left", WIDTH()/2-30 ],[spine_btn, "bottom",HEIGHT()/2+60]])
+cmds.formLayout(model_tab, e=True, af=[[waist_btn,  "left", WIDTH()/2-35 ],[waist_btn, "bottom",HEIGHT()/2]])
+cmds.formLayout(model_tab, e=True, af=[[pelvis_btn, "left", WIDTH()/2-45 ],[pelvis_btn, "top",HEIGHT()/2]])
+cmds.formLayout(model_tab, e=True, af=[[master_btn, "left", WIDTH()/2-100],[master_btn, "bottom",60]])
 
 # Setup the reset buttons
-resetall_button = cmds.button(label="Reset All",      w=WIDTH()/2-20, command="pass")
-resetsel_button = cmds.button(label="Reset Selected", w=WIDTH()/2-20, command="pass")
-cmds.formLayout(model_tab, e=True, af=[[resetsel_button,"left",          10],[resetsel_button,"top",HEIGHT()-60]])
-cmds.formLayout(model_tab, e=True, af=[[resetall_button,"left",WIDTH()/2+10],[resetall_button,"top",HEIGHT()-60]])
+resetall_btn = cmds.button(label="Reset All",      w=WIDTH()/2-20, command="pass")
+resetsel_btn = cmds.button(label="Reset Selected", w=WIDTH()/2-20, command="pass")
+cmds.formLayout(model_tab, e=True, af=[[resetsel_btn,"left",  10],[resetsel_btn,"bottom",10]])
+cmds.formLayout(model_tab, e=True, af=[[resetall_btn,"right", 10],[resetall_btn,"bottom",10]])
 
-## Setup the foot tab
+## Setup the foot tab ##
 cmds.setParent( tab_layout )
 feet_tab = cmds.columnLayout( width=WIDTH() )
 
 hl_grp = cmds.floatSliderGrp( l='Heel Lift', field=True, min=-20, max=20, value=0, cw=[(1,80),(2,60),(3,WIDTH()-160)] )
 #cmds.connectControl( hl_grp, '%s.Custom' % 'pSphere1' )
 
-
-## Setup the hands tab
+## Setup the hands tab ##
 cmds.setParent( tab_layout )
 hands_tab = cmds.columnLayout( width=WIDTH(), adjustableColumn=True )
 
